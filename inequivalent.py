b38f20d877505160c2ab01ae0d242153da0c8c5e
Olivier Sobrie <olivier@sobrie.be>
2014-08-22 08:26:14 +0200

    inequivalent: add a function that count inequivalent MBFs

---
+++ inequivalent.py
@@ -0,0 +1,48 @@
+import itertools
+
+def compute_permutations(variables):
+    return [dict(zip(variables, i))
+            for i in itertools.permutations(variables, len(variables))][1:]
+
+def permute_indices(mbf, perm):
+    f = lambda i: perm[i]
+
+    mbfeq = set()
+    for coa in mbf:
+        coaeq = frozenset(map(f, coa))
+        mbfeq.add(coaeq)
+
+    return mbfeq
+
+def find_inequivalent_mbfs(mbfs, perms):
+    imbfs = dict()
+
+    while len(mbfs) > 0:
+        mbf = mbfs.pop()
+        imbfs[mbf] = 1
+
+        for perm in perms:
+            mbfeq = permute_indices(mbf, perm)
+            for mbf2 in mbfs.copy():
+                if mbfeq == mbf2:
+                    mbfs.discard(mbf2)
+                    imbfs[mbf] += 1
+
+    return imbfs
+
+if __name__ == "__main__":
+    import sys
+    from mbf import compute_all_coalitions
+    from mbf import mbf_from_profiles
+
+    variables = (1, 2, 3, 4)
+    profile = (0, 3, 0, 0)
+
+    combis = compute_all_coalitions(variables, [profile])
+    mbfs = mbf_from_profiles(variables, profile, combis)
+
+    perms = compute_permutations(variables)
+    print(perms)
+
+    imbfs = find_inequivalent_mbfs(mbfs, perms)
+    print(imbfs)
