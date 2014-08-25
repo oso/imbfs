3053cf7943390746bbeba3717ffaf9af318a35b7
Olivier Sobrie <olivier@sobrie.be>
2014-08-25 10:40:14 +0200

    utils: add powerset() function

--- utils.py
+++ utils.py
@@ -1,4 +1,10 @@
 import resource
+from itertools import chain, combinations
 
 def cpu_time():
     return sum(resource.getrusage(resource.RUSAGE_SELF)[:2])
+
+def powerset(iterable):
+    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
+    s = list(iterable)
+    return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
