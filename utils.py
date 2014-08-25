7294793167ca48e3686273d9400cffa7d497d1c8
Olivier Sobrie <olivier@sobrie.be>
2014-08-25 10:40:24 +0200

    utils: add mbf_to_str() function

--- utils.py
+++ utils.py
@@ -4,6 +4,16 @@ from itertools import chain, combinations
 def cpu_time():
     return sum(resource.getrusage(resource.RUSAGE_SELF)[:2])
 
+def mbf_to_str(mbf):
+    string = "MBF("
+    if len(mbf) == 0:
+        return string + ")"
+
+    for s in mbf:
+        string += "[" + ", ".join(map(str, s)) + "], "
+    string = string[:-2] + ")"
+    return string
+
 def powerset(iterable):
     "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
     s = list(iterable)
